2bbd46ddc93604fa917ba42c31742b67d65e7eb1
David Zhao (DEVDIV) <dazhao@microsoft.com>
2017-06-02 01:03:06 -0700

    Adds support for Python files.

---
+++ FormatPendingChanges/DocumentActions/PythonFormatDocumentAction.cs
@@ -0,0 +1,25 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+
+namespace FormatPendingChanges.DocumentActions
+{
+    [Export(typeof(DocumentAction))]
+    internal sealed class PythonFormatDocumentAction : FormatDocumentAction
+    {
+        private static readonly string[] PythonFileExtensions = { ".py" };
+
+        [ImportingConstructor]
+        public PythonFormatDocumentAction(SVsServiceProvider serviceProvider)
+            : base(serviceProvider)
+        {
+        }
+
+        public override bool CanExecute(ProjectItem projectItem)
+        {
+            return projectItem != null && PythonFileExtensions.Any(p => projectItem.Name.EndsWith(p, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
